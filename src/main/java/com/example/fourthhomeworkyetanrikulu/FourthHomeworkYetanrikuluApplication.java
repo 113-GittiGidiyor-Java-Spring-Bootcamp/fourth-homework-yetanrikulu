751f5bbe1d14be9d0fea4b5e552e3294420f8955
Yaemta <yaemta@gmail.com>
2021-09-06 14:25:16 +0300

    first commit

---
+++ src/main/java/com/example/fourthhomeworkyetanrikulu/FourthHomeworkYetanrikuluApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourthhomeworkyetanrikulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthHomeworkYetanrikuluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthHomeworkYetanrikuluApplication.class, args);
+    }
+
+}
